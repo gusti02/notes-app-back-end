3ee4041406e596c95b71535387a2bf3448deed17
Gusti <gustirizki26@gmail.com>
2023-10-30 21:10:40 +0700

    Second commit

--- src/server.js
+++ src/server.js
@@ -1,2 +1,17 @@
 /* eslint-disable no-console */
-console.log('Hallo kita akan membuat RESTful API');
+
+// membuat web server menggunakan hapi
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+  });
+
+  
+  await server.start();
+  console.log(`Server sedang berjalan pada ${server.info.uri}`);
+};
+
+init();
