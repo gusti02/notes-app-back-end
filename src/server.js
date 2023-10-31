731535ed439507336ac75401fa4fea64981c4b0b
gusti <gustirizki26@gmail.com>
2023-10-31 10:11:25 +0700

    menerapkan CORS untuk semua data

--- src/server.js
+++ src/server.js
@@ -8,6 +8,11 @@ const init = async () => {
   const server = Hapi.server({
     port: 5000,
     host: 'localhost',
+    routes: {
+      cors: {
+        origin: ['*'] // menerapkan CORS pada webserver 
+      }
+    },
   });
 
   // menjalankan routing
