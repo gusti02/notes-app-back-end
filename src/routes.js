653c57eba5a09b425a8b99d1107ad4bd0dc0351b
gusti <gustirizki26@gmail.com>
2023-10-31 10:10:52 +0700

    memnambahkan method GET dan GET by ID

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,27 @@
-const { addNoteHandler } = require('./handler');
+const { addNoteHandler, getAllNotesHandler, getNoteByIdHandler } = require('./handler');
 
 // membuat routing yang berisi array
 const routes = [
   {
+    // ini untuk membuat note baru
     method: 'POST',
     path: '/notes',
     handler: addNoteHandler,
   },
+
+  {
+    // ini untuk menampilkan seluruh note
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler,
+  },
+
+  {
+    // menampilkan atau mengambil note secara detail dengan id
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNoteByIdHandler,
+  }
 ];
 
 // export module route
